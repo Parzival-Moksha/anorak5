f0d4d1eb6870c34e363c51e940fb7330c6dd356b
Parzival-Moksha <leviturk@protonmail.com>
2024-12-04 11:43:03 +0100

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import ClientWalletProvider from './providers/WalletProvider'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+  <ClientWalletProvider>
+    {children}
+  </ClientWalletProvider>
+</body>
     </html>
   );
 }
